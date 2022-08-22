414224bda81ee491e2d1840c18a36b78b9701e87
anejpodkriznik <88827298+anejpodkriznik@users.noreply.github.com>
2022-08-22 10:22:42 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-    $user = 'root';
-    $pass = '';
-    $server = 'localhost';
-    $db_name = 'slovenskerestavracije';
+    $user = 'b452c9d3e65c15';
+    $pass = '867b1917';
+    $server = 'eu-cdbr-west-03.cleardb.net';
+    $db_name = 'heroku_804462f04350d2d';
 
     //$link = mysqli_connect($server, $user, $pass, $db_name);
 	//mysqli_query($link, "SET NAMES 'utf8'");
